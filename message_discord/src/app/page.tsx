a308f168170c24c6b5bf3f18d176eceb25d63bc6
Basil Njoga <Basilnjoga@gmail.com>
2023-07-01 14:42:40 +0300

    created form and added tailwind css

---
+++ message_discord/src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <>
+    <header>
+    <h1 className="text-3xl font-bold underline">
+      Homework
+    </h1>
+      </header>
+      <div className="border border-gray-700" />
+      <form action="/#" method="post">
+  
+  <input type="text" id="first" name="first" />
+  <div><button class="bg-sky-500 hover:bg-sky-700" type="submit" >Send message</button></div>
+  
+</form>
+   
+    </>
+  )
+  
+}
